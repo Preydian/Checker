2aa5117e20207ff98e0eac7fb352e4e1dd265488
Preydian <raithfullam0221@gmail.com>
2023-12-29 01:03:54 +1300

    Added oAuth

--- src/components/checker.tsx
+++ src/components/checker.tsx
@@ -5,45 +5,42 @@ import {useLocation} from "react-router-dom";
 
 const Checker = () => {
 
+    const authUrl = `https://www.bungie.net/en/oauth/authorize?client_id=45985&response_type=code`;
+    const location = useLocation();
     const [authCode, setAuthCode] = React.useState("First")
 
-    const BungieOAuthCallback: React.FC = () => {
-        const location = useLocation();
 
-        useEffect(() => {
-            // Extract the authorization code from the query parameters
-            const urlSearchParams = new URLSearchParams(location.search);
-            const authorizationCode = urlSearchParams.get('code');
+    React.useEffect(() => {
 
-            if (authorizationCode) {
-                // Exchange authorization code for access token
-                const tokenUrl = 'https://www.bungie.net/platform/app/oauth/token/';
-                const tokenParams = {
-                    client_id: '45985',
-                    client_secret: '6siat6XMy81e.SF4ODpsot78zfI9G5zfG9npwAaJjK4',
-                    code: authorizationCode,
-                    grant_type: 'authorization_code',
-                    redirect_uri: 'https://destinyqueuechecker.bot.nu/',
-                };
+        // Extract the authorization code from the query parameters
+        const urlSearchParams = new URLSearchParams(location.search);
+        const authorizationCode = urlSearchParams.get('code');
 
-                axios
-                    .post(tokenUrl, null, { params: tokenParams })
-                    .then(response => {
-                        const accessToken = response.data.access_token;
-                        // Save the access token in your application state or use it as needed
-                        console.log('Access Token:', accessToken);
-                        setAuthCode(response.data)
-                    })
-                    .catch(error => {
-                        console.error('Error getting access token:', error);
-                    });
-            }
-        }, [location.search]);
+        if (authorizationCode) {
+            // Exchange authorization code for access token
+            const tokenUrl = 'https://www.bungie.net/platform/app/oauth/token/';
+            const tokenParams = {
+                client_id: '45985',
+                client_secret: '6siat6XMy81e.SF4ODpsot78zfI9G5zfG9npwAaJjK4',
+                code: authorizationCode,
+                grant_type: 'authorization_code',
+                redirect_uri: 'https://destinyqueuechecker.bot.nu/',
+            };
 
-        return <div>Processing...</div>;
-    };
+            axios
+                .post(tokenUrl, null, { params: tokenParams })
+                .then(response => {
+                    const accessToken = response.data.access_token;
+                    // Save the access token in your application state or use it as needed
+                    console.log('Access Token:', accessToken);
+                    setAuthCode(response.data)
+                })
+                .catch(error => {
+                    console.error('Error getting access token:', error);
+                });
+        }
 
-    const authUrl = `https://www.bungie.net/en/oauth/authorize?client_id=45985&response_type=code`;
+    }, [location.search]);
 
 
     return (

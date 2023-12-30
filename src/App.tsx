ea2e77bdd2d3ea69e26f3b841424d43d8f92d827
Preydian <raithfullam0221@gmail.com>
2023-12-30 22:32:24 +1300

    Updated website title

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-// Install necessary dependencies:
-// npm install axios react-router-dom
-
 import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Checker from "./components/checker";
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Checker/>}/>
-                {/* Add other routes for your application */}
-            </Routes>
-        </Router>
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Checker/>}/>
+                </Routes>
+            </Router>
+        </div>
     );
 };
 
